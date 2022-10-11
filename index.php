8f26af5d924d618b7ef51af28ebd746ec8927ad9
OthmanAlkhatib <97898775+OthmanAlkhatib@users.noreply.github.com>
2022-10-11 13:32:15 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    // header("Content-Type:application/json; charset=utf-8");
+    // include("./connect.php");
+    header('Access-Control-Allow-Origin: *');
+ 
+    // $sql_select = "SELECT * FROM `products`";
+    // $result = $con->query($sql_select);
+    // $sql_data = array();
+    
+    // While($row = $result->fetch_assoc()) {
+    //     $sql_data[] = $row;
+    // }
+
+    // $product_url = $sql_data[0]['product_url'];
+    $api_url = "https://softcare.sy:8001/api/get-product?url=https://www.amazon.in/3M-08578-Black-Strip-Calk/dp/B000PEZ1L4";
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $api_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    
+    $server_data = curl_exec($ch);
+    curl_close($ch);
+    var_dump(curl_error($ch));
+    echo $server_data;
+ ?>
+
+<html>
+  <head>
+    <title>hello php</title>
+  </head>
+  <body>
+    hello php
+  </body>
+</html>
+
+<script>
+    console.log('hi');
+</script>
